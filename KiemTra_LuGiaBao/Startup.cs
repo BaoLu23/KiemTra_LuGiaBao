598efcbae16dcd04b5cc94405e6b6c096e346ebf
baobl <baobl@DESKTOP-GR5USKA>
2022-06-13 13:16:01 +0700

    Add project files.

---
+++ KiemTra_LuGiaBao/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(KiemTra_LuGiaBao.Startup))]
+namespace KiemTra_LuGiaBao
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
